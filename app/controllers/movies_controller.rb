3db78299874d939352ac990cb4a2782e67d06ece
Devin Mullin <devin.mullin90@gmail.com>
2021-11-03 11:12:56 -0500

    commit tree 3c3bf61e745c57c680db1677e3a6003e35817038

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,4 +5,18 @@ class MoviesController < ApplicationController
     render json: movies
   end
 
+  def create
+    movie = Movie.create(movie_params)
+    render json: movie, status: :created
+  rescue ActiveRecord::RecordInvalid => invalid
+    render json: {errors: movie.errors.full_messages}, status: :unprocessable_entity
+  end
+
+
+  private
+  
+  def movie_params
+    params.permit(:title, :year, :length, :director, :description, :poster, :category, :female_director, :discount)
+  end
+
 end
